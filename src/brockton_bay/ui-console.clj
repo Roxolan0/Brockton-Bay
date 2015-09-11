0bd0ce2bcca582641e643b916581dad9ccbe4ae9
Hugo Jacob <jacobhugos@gmail.com>
2015-09-11 16:28:08 +0100

    Began work on console menus.

--- src/brockton_bay/ui-console.clj
+++ src/brockton_bay/ui-console.clj
@@ -1,3 +1,23 @@
 (ns brockton-bay.ui-console
   (:use brockton-bay.main))
 
+(defrecord Option [message effect])
+
+(defn ask [message options]
+  {:pre [(instance? clojure.lang.PersistentArrayMap options)
+         (not-empty options)]}
+  (let [numbered-options (zipmap (range (count options)) options)]
+    (println message)
+    (map
+      #(println (str "- " (:message (second %)) " (" (first %) ")"))
+      numbered-options)
+    (->
+      "0"                     ;TODO should be (readline)
+      (. Integer parseInt)
+      (find numbered-options)
+      (:effect)
+      )))
+
+(defrecord play-game []
+
+  )
